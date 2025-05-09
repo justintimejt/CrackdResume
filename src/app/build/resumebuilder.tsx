0f47e7c31dd526f72be6e9c286dc6b7ed96a0bdd
justintimejt <justinchow3607@gmail.com>
2025-05-09 13:21:28 -0400

    await onClick submission button and render loading screen

--- src/app/build/resumebuilder.tsx
+++ src/app/build/resumebuilder.tsx
@@ -4,17 +4,19 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import TemplateSelector from "../components/TemplateSelector";
 import Form from "../components/Form";
+import LoadingPage from "../components/LoadScreen";
 
 export default function ResumeBuilder() {
   const [selectedTemplate, setSelectedTemplate] = useState<null | {
     name: string;
     image: string;
   }>(null);
+  const [loading, setLoading] = useState(false);
 
   const router = useRouter();
 
   const handleFormComplete = async (latex: string) => {
-    router.push("/loading");
+    setLoading(true);
 
     try {
       const compileRes = await fetch("/api/compile-pdf", {
@@ -34,7 +36,9 @@ export default function ResumeBuilder() {
 
   return (
     <div className="p-8">
-      {!selectedTemplate ? (
+      {loading ? (
+        <LoadingPage/> //render loading screen
+      ) : !selectedTemplate ? (
         <TemplateSelector onSelect={setSelectedTemplate} />
       ) : (
         <Form selectedTemplate={selectedTemplate} 
