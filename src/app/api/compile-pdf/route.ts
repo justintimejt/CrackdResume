5406aa00df743a18f7c85488b60bccfb3b7626fd
justintimejt <justinchow3607@gmail.com>
2025-05-13 20:33:53 -0400

    fix: implement debugging for compile-pdf endpoint

--- src/app/api/compile-pdf/route.ts
+++ src/app/api/compile-pdf/route.ts
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs/promises';
+import { writeFile, access } from 'fs/promises';
 import { exec } from 'child_process';
 import { promisify } from 'util';
 import { NextResponse } from 'next/server';
@@ -7,18 +7,35 @@ import { v4 as uuidv4 } from 'uuid';
 const execAsync = promisify(exec);
 
 export async function POST(req: Request) {
+    console.log('PDF compilation request received');
+
     const { latex } = await req.json();
 
     const id = uuidv4();
     const texPath = `/tmp/${id}.tex`;
+    const pdfPath = `/tmp/${id}.pdf`;
+
     const pdfDir = `/tmp`;
 
-    try {
-        await writeFile(texPath, latex);
+    console.log("PDF ID:", id);
+    console.log(`Writing LaTeX to: ${texPath}`);
+    await writeFile(texPath, latex);
 
+
+
+
+    try {
         //COMPILE LATEX TO PDF using pdflatex
-        await execAsync(`/Library/TeX/texbin/pdflatex -output-directory=${pdfDir} ${texPath}`);
         console.log("Compiled PDF ID:", id);
+
+        const { stdout, stderr } = await execAsync(
+            `/Library/TeX/texbin/pdflatex -output-directory=${pdfDir} ${texPath}`
+        )
+        console.log('pdflatex stdout:', stdout);
+        console.log('pdflatex stderr:', stderr);
+
+        await access(pdfPath);
+        console.log(`PDF successfully created at: ${pdfPath}`);
         return NextResponse.json({ id });
     } catch (error) {
         console.log("PDF Compile Error: ", error);
