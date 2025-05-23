f01ecc58f26fdb54f4fd55d7f3b15da24db613d4
justintimejt <justinchow3607@gmail.com>
2025-05-22 21:20:23 -0400

    feat: completed prototype navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-black min-h-screen">
-      <nav className ="w-7/8 mx-auto sticky top-4 bg-white rounded-3xl shadow-md p-4 z-50 flex justify-between items-center">
-          <button className="text-lg font-semibold">Source Code</button>
-          <button className="text-lg font-semibold"><a href="/">Gen Now</a></button>
+      <nav className ="w-19/20 mx-auto sticky top-4 bg-black rounded-3xl shadow-md p-4 z-50 flex justify-between items-center">
+
+        <div className="text-lg font-bold text-white">
+          resumeapp
+        </div>
+        <div className="flex space-x-4">
+          <button className="text-md text-white">Source Code</button>
+          <button className="text-md text-black bg-white p-2 rounded-md"><a href="/">Gen Now</a></button>
+        </div>
+          
 
       </nav>
       <main className="mt-24">
