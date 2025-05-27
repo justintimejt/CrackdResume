f0b70f453e9abd70338da8cda435e6bb763c9ea1
justintimejt <justinchow3607@gmail.com>
2025-05-27 14:12:50 -0400

    fix: minor ui fixes on result page

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -36,25 +36,32 @@ export default function ResultPage () {
     if (loading) return <div>Loading preview...</div>;
 
     return (
-        <div className="flex flex-col items-center px-4 py-6">
-            {/* <Preview latex={latex} /> */}
-      
-            <div className="w-full max-w-4xl h-[70vh] border shadow-lg">
-                <iframe 
-                    src={pdfUrl}
-                    className="w-full h-full rounded-md"
-                    title="PDF Preview"
-                />
-            </div>
+        <div className ='bg-black min-h-screen'>
+            <div className="flex flex-col items-center px-4 py-6">
+                <h1 className ='text-3xl md:text-4xl font-semibold text-center mb-6 text-white drop-shadow-[0_0_10px_rgba(59,130,246,0.7)]'>
+                    Your Resume is Ready!
+                </h1>
+                <p className='text-center text-gray-300 text-lg mb-8 max-w-2xl mx-auto'>
+                    Download as a pdf or tex file - happy job hunting :/
+                </p>
+        
+                <div className="w-full max-w-4xl h-[70vh] border shadow-lg">
+                    <iframe 
+                        src={pdfUrl}
+                        className="w-full h-full rounded-md"
+                        title="PDF Preview"
+                    />
+                </div>
 
-            {/*Download buttons*/}
-            <div className="mt-6 flex flex-wrap gap-4">
-                <a href={pdfUrl} download="resume.pdf">
-                    <button className="px-4 py-4 bg-blue-600 text-white rounded hover:bg-blue-700">Download PDF</button>
-                </a>
-                <a href={texUrl} download="resume.tex">
-                    <button className="px-4 py-4 bg-green-600 text-white rounded hover:bg-green-700">Download .tex</button>
-                </a>
+                {/*Download buttons*/}
+                <div className="mt-6 flex flex-wrap gap-4">
+                    <a href={pdfUrl} download="resume.pdf">
+                        <button className="px-4 py-4 bg-blue-600 text-white rounded hover:bg-blue-700">Download PDF</button>
+                    </a>
+                    <a href={texUrl} download="resume.tex">
+                        <button className="px-4 py-4 bg-green-600 text-white rounded hover:bg-green-700">Download .tex</button>
+                    </a>
+                </div>
             </div>
         </div>
     );
